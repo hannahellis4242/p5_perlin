f2ebfd1d2cd64f102c564ab117686cd3f91fb4ac
Hannah Ellis <hannahellis4242@gmail.com>
2022-03-18 22:47:48 +0000

    These values seem nice

--- client/src/index2.ts
+++ client/src/index2.ts
@@ -5,14 +5,14 @@ const sketch = (p5: P5) => {
   // The sketch setup method
   p5.setup = () => {
     // Creating and positioning the canvas
-    const canvas = p5.createCanvas(800, 500);
+    const canvas = p5.createCanvas(200, 300);
     canvas.parent("app");
-    p5.noiseDetail(8, 0.2);
+    //p5.noiseDetail(8, 0.5);
   };
 
-  const xStep = 0.001;
-  const yStep = 0.001;
-  const zStep = 0.001;
+  const xStep = 0.0125;
+  const yStep = 0.0125;
+  const zStep = 0.0125;
   let z = 0;
   // The sketch draw method
   p5.draw = () => {
@@ -24,10 +24,12 @@ const sketch = (p5: P5) => {
           for (let j = 0; j < d; j++) {
             // loop over
             const index = 4 * ((y * d + j) * p5.width * d + (x * d + i));
-            const value = 255 * p5.noise(x * xStep, y * yStep, z);
-            p5.pixels[index] = value;
-            p5.pixels[index + 1] = value;
-            p5.pixels[index + 2] = value;
+            const r = 255 * p5.noise(x * xStep, y * yStep, z);
+            const g = 255 * p5.noise((x + p5.width) * xStep, y * yStep, z);
+            const b = 255 * p5.noise(x * xStep, (y + p5.height) * yStep, z);
+            p5.pixels[index] = r;
+            p5.pixels[index + 1] = g;
+            p5.pixels[index + 2] = b;
             p5.pixels[index + 3] = 255;
           }
         }
